db186bf3a3bf9cb4fb8e29aa352157415bb9b2e8
ArnauGD <114002201+ArnauGD@users.noreply.github.com>
2022-10-23 12:17:06 +0200

    Add files via upload

---
+++ Powershell_Act1_Script2_Arnau_Garcia.ps1
@@ -0,0 +1,21 @@
+﻿#Agafem els parametres, el directori origen amb la seva ruta absoluta
+#i el directori destí on es copiarà
+
+Param(
+[string] $origen,
+[string] $destí
+)
+
+#Agafem el string de la ruta origen i el separem per després agafar el nom de la carpeta que volem copiar
+
+$array_origen=$origen.Split("\")
+
+$nom_fitxer = $array_origen[-1]
+
+#Obtenim la data en la que és fa el backup
+
+$backup_date = Get-Date -Format dd-MM-yyyy
+
+#Copiem el directori a la ruta destí, amb el nom de format Backup_nomfitxer_data
+
+Copy-Item -Path $origen -Destination ($destí+"\Backup_"+$nom_fitxer+"_"+$backup_date)
